2485175a428f1d0ec416a80f5a7f443f00b7df8d
Lucas da Silva <sklucassilva@gmail.com>
2023-02-28 14:31:22 -0300

    Refatoração de codigo realizado

--- robo-ocr.py
+++ robo-ocr.py
@@ -7,9 +7,11 @@ import cv2
 from time import sleep
 
 caminho = r"C:\Program Files\Tesseract-OCR"
-entrada = 'Entrada'
-green = 'GREEN'
-red = 'Red'
+palavraEntrada = 'Entrada'
+palavraGreen = 'GREEN'
+palavraFogueteConfirmado = 'Foguetinho confirmado'
+palavraRed = 'Red'
+palavraPossivelEntrada = 'ATENÇÃO, POSSÍVEL ENTRADA'
 
 pytesseract.pytesseract.tesseract_cmd = caminho + r"\tesseract.exe"
 
@@ -27,10 +29,10 @@ def extrairImagem():
 
         # The screen part to capture
         monitor = {
-            "top": 650,
+            "top": 770,
             "left": 3250,
-            "width": 572,
-            "height": 376,
+            "width": 558,
+            "height": 230,
             "mon": monitor_number,
         }
         #output = "sct-mon{mon}_{top}x{left}_{width}x{height}.png".format(**monitor)
@@ -43,50 +45,28 @@ def extrairImagem():
         mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
         print(output)
         sleep(1)
-  
-def lerConteudoImagem():
+    
+while (True):
+    extrairImagem()    
+    
     imagem = cv2.imread("entrada.png")
     # Converte a imagem para o formato de texto usando o pytesseract
     texto = pytesseract.image_to_string(imagem, lang="por")
 
-    valorOdd = re.findall(r'\d+\.\d+', texto)    
-    valorOdd = float(valorOdd[1])
+    confirmacaoFoguete = re.findall(r'\bFoguetinho confirmado\b', texto)    
+    entrada = re.findall(r'\bEntrada\b', texto) 
+    odd = re.findall(r'\d+\.\d+', texto)   
     
-    if(valorOdd == None or valorOdd == ''):
-        valorOdd = ''
-    
-    palavraEntrada = re.findall(r'Entrada', texto)
-    palavraEntrada = palavraEntrada[0]
-    
-    if(palavraEntrada == None or palavraEntrada == ''):
-        palavraEntrada = ''
-    
-    palavraGreen = re.findall(r'GREEN', texto)
-    palavraGreen = palavraGreen[0]
-
-    if(palavraGreen == None or palavraGreen == ''):
-     palavraGreen = ''
-    
-
-    data = {"odd": valorOdd, "entrada": palavraEntrada, "green": palavraGreen}
-    print(data)
-    return data
-
-def verificarGreen():
-    while (True):
-        extrairImagem()
-        lerConteudoImagem()
-        if(data['green'] == green):
-            break           
-        else:
-              print('VERIFICANDO GREEN PARA CONTINUAR')
-    
-while (True):
-    extrairImagem()    
-    data = lerConteudoImagem()
-    if(data['entrada'] == entrada):   
+    if  len(confirmacaoFoguete) != 0 and len(entrada) != 0 and len(odd) != 0:
+        variavelFoguete = confirmacaoFoguete[0]
+        variavelEntrada = entrada[0] 
+        variavelOdd = odd[0]
+          
+        print(variavelFoguete)
+        print(variavelEntrada)
+        print(variavelOdd)
+        
         jogar()
-        verificarGreen()
-    else:    
-        print('Esperando ENTRADA ')
+    else:
+        print("ESPERANDO ENTRADA")
     
